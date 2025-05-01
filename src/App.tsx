041376273c1fd0efefd826cede588556dd86ba5c
HitomiTachi <lethanhnhanminer@gmail.com>
2025-05-02 05:03:18 +0700

    Frontend_update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='bg-primary text-white p-5'>
+        <h1>Welcome to the Manager Class Project</h1>
+        <p>This is a simple React application.</p>
+      </div>
+      MANAGER CLASS PROJECT
     </div>
   );
 }
