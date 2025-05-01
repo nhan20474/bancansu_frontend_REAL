9034764d155abb84c7ddef01f877baeaee814b26
HitomiTachi <lethanhnhanminer@gmail.com>
2025-05-02 06:32:22 +0700

    Giao diện thay đổi tí

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='bg-primary text-white p-5'>
-        <h1>Welcome to the Manager Class Project</h1>
-        <p>This is a simple React application.</p>
-      </div>
-      MANAGER CLASS PROJECT
+    <div className="layout">
+      <aside className="sidebar">
+        <div className="logo"> CLASS MANAGER</div>
+        <ul className="menu">
+          <li><i className="fas fa-home"></i> Trang chủ</li>
+          <li><i className="fas fa-user"></i> Hồ sơ cá nhân</li>
+          <li><i className="fas fa-book"></i> Tài liệu</li>
+          <li><i className="fas fa-users"></i> Diễn đàn</li>
+          <li><i className="fas fa-clipboard-list"></i> Khảo sát</li>
+          <li><i className="fas fa-check-circle"></i> Rèn luyện</li>
+          <li><i className="fas fa-question-circle"></i> Trợ giúp</li>
+          <li><i className="fas fa-solid fa-ellipsis"></i> Khác</li>
+          <li><i className="fas fa-cogs"></i> Tài khoản</li>
+        </ul>
+      </aside>
+
+      <main className="main">
+        <header className="main-header">
+          <h2>WELCOME</h2>
+          <p>Chào mừng đến với cổng sinh viên HUTECH</p>
+        </header>
+      </main>
     </div>
   );
 }
